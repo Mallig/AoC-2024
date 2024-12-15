365bfa2c29c67066e2c28be13d781f5346d93fbf
Malachy Gilchrist <malachygilchrist@gmail.com>
2024-12-15 19:03:30 +0000

    remove sub module architecture

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"AdventOfCode/day_1"
+)
+
+func main() {
+	day_1.Complete("./inputs/day_1.txt")
+}
--- solve/go.mod
+++
@@ -1,7 +0,0 @@
-module solve
-
-go 1.23.4
-
-replace day_1 => ../day_1
-
-require day_1 v0.0.0-00010101000000-000000000000
